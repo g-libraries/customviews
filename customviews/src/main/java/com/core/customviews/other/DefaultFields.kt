caa342e319504bfd23f7a083d626b31e537b90b3
Roman Kokosh <romakokosh36@gmail.com>
2020-10-27 13:13:28 +0200

    DefaultFields update

--- customviews/src/main/java/com/core/customviews/other/DefaultFields.kt
+++ customviews/src/main/java/com/core/customviews/other/DefaultFields.kt
@@ -1,5 +1,5 @@
 package com.core.customviews.other
 
 enum class FieldType {
-    edit_profile, about_us, orders, push_notifications, tech_support, exit
+    edit_profile, about_us, orders, push_notifications, private_notifications, tech_support, exit
 }
