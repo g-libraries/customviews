011a914cbb952db908450bb231a1406e40457313
Roman Roschin <rroschin@gbsfo.com>
2021-02-04 12:04:40 +0200

    change minimum name length to 2 in Validator validateNameFun

--- customviews/src/main/java/com/core/customviews/profile/Validator.kt
+++ customviews/src/main/java/com/core/customviews/profile/Validator.kt
@@ -5,7 +5,7 @@ import androidx.core.util.PatternsCompat
 class Validator {
     val validateNameFun: (String?) -> Boolean = { name ->
         if (!name.isNullOrEmpty()) {
-            name.trim().length > 2
+            name.trim().length >= 2
         } else
             false
     }
