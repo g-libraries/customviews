48e9a6980b6d89093e80548d9c1d968f3bd6fa3e
cr9ck <andreyarosh29@gmail.com>
2021-03-18 18:49:16 +0200

    bugfix: libraries version updates

--- customviews/src/main/java/com/core/customviews/login/PhoneNumberEditText.kt
+++ customviews/src/main/java/com/core/customviews/login/PhoneNumberEditText.kt
@@ -12,9 +12,9 @@ import timber.log.Timber
 import java.lang.IndexOutOfBoundsException
 
 class PhoneNumberEditText : AppCompatEditText {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
